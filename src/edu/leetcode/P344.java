90c0dbb96f10acf759e93c6f1faea25396cd3a29
Nawara <nawarat@amazon.com>
2017-10-17 15:34:21 +0200

    finished implementing problem 344

---
+++ src/edu/leetcode/P344.java
@@ -0,0 +1,19 @@
+package edu.leetcode;
+
+public class P344 {
+    public static void main(String[] args) {
+        Solution solver = new Solution();
+        final String result = solver.reverseString("hello");
+        System.out.println(result);
+    }
+
+    private static class Solution {
+        public String reverseString(String s) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = s.length() - 1; i >= 0; --i) {
+                sb.append(s.charAt(i));
+            }
+            return sb.toString();
+        }
+    }
+}
