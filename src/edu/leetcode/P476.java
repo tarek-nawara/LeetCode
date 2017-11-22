aed7945f4f223af55c80efa2d5bf3c3581b070f0
Tarek <tarek.nawara@gmail.com>
2017-11-22 19:18:21 +0200

    solved problem 476

---
+++ src/edu/leetcode/P476.java
@@ -0,0 +1,21 @@
+package edu.leetcode;
+
+public class P476 {
+    public static void main(String[] args) {
+        Solution solver = new Solution();
+        final int res = solver.findComplement(5);
+        System.out.println(res);
+    }
+
+    private static class Solution {
+        public int findComplement(int num) {
+            StringBuilder sb = new StringBuilder();
+            while (num > 0) {
+                int mod = num % 2;
+                sb.append(mod == 1 ? 0 : 1);
+                num /= 2;
+            }
+            return Integer.parseInt(sb.reverse().toString(), 2);
+        }
+    }
+}
