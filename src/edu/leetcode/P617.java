c83b94f55d5046849edc1d69861eff25d4721af6
Tarek <tarek.nawara@gmail.com>
2017-11-22 18:57:02 +0200

    solved problem 617

---
+++ src/edu/leetcode/P617.java
@@ -0,0 +1,31 @@
+package edu.leetcode;
+
+public class P617 {
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+
+        TreeNode(int x, TreeNode left, TreeNode right) {
+            this.left = left;
+            this.right = right;
+            this.val = val;
+        }
+    }
+
+    class Solution {
+        public TreeNode mergeTrees(TreeNode rootOne, TreeNode rootTwo) {
+            if (rootOne == null && rootTwo == null) return null;
+            if (rootOne == null) return rootTwo;
+            if (rootTwo == null) return rootOne;
+            final TreeNode newRoot = new TreeNode(rootOne.val + rootTwo.val);
+            newRoot.left = mergeTrees(rootOne.left, rootTwo.left);
+            newRoot.right = mergeTrees(rootOne.right, rootTwo.right);
+            return newRoot;
+        }
+    }
+}
