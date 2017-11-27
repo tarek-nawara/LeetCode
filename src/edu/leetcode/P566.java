9a6aea9ad1c285cc89b7ba6fc14f5a14afbb0be4
tarek-nawara <tarek.nawara@gmail.com>
2017-11-27 18:15:00 +0200

    solved P566

---
+++ src/edu/leetcode/P566.java
@@ -0,0 +1,38 @@
+package edu.leetcode;
+
+import java.util.Arrays;
+
+public class P566 {
+    public static void main(String[] args) {
+        Solution solver = new Solution();
+        int[][] nums = {{1, 2}, {3, 4}};
+        final int[][] res = solver.matrixReshape(nums, 4, 1);
+        for (int i = 0; i < res.length; ++i) {
+            for (int j = 0; j < res[i].length; ++j) {
+                if (j > 0) System.out.print(" ");
+                System.out.print(res[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    private static class Solution {
+        public int[][] matrixReshape(int[][] nums, int r, int c) {
+            if (nums == null || nums.length == 0) return nums;
+            int n = 0;
+            for (int i = 0; i < nums.length; ++i) {
+                n += nums[i].length;
+            }
+            if ((long) r * c != n) return nums;
+            int[][] res = new int[r][c];
+            int idx = 0;
+            for (int i = 0; i < r; ++i) {
+                for (int j = 0; j < c; ++j) {
+                    res[i][j] = nums[idx / nums[0].length][idx % nums[0].length];
+                    ++idx;
+                }
+            }
+            return res;
+        }
+    }
+}
